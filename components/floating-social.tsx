3b1660398c1abc60e965c7477f64342f7a8f0c4f
risik34 <risimn7@gmail.com>
2025-02-24 09:48:32 +0000

    update floating social links

--- components/floating-social.tsx
+++ components/floating-social.tsx
@@ -32,9 +32,15 @@ const FloatingSocial = () => {
           transition={{ duration: 0.5, ease: "linear" }}
           className="fixed bottom-0 left-4 flex flex-col items-center space-y-4"
         >
-          <IconRenderer src="/icons/github.svg" />
-          <IconRenderer src="/icons/twitter.svg" />
-          <IconRenderer src="/icons/linkedin.svg" />
+          <IconRenderer
+            href="https://github.com/risicoding"
+            src="/icons/github.svg"
+          />
+          <IconRenderer
+            href="https://www.x.com/risi_ksh"
+            src="/icons/twitter.svg"
+          />
+          <IconRenderer href="" src="/icons/linkedin.svg" />
           <div className="h-28 w-[1px] bg-white" />
         </motion.div>
       )}
@@ -42,8 +48,10 @@ const FloatingSocial = () => {
   );
 };
 
-const IconRenderer = ({ src }: { src: string }) => (
-  <Image src={src} width={20} height={20} alt={src} />
+const IconRenderer = ({ src, href }: { src: string; href: string }) => (
+  <a href={href}>
+    <Image src={src} width={20} height={20} alt={src} />
+  </a>
 );
 
 export default FloatingSocial;
